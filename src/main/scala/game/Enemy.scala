b7b8247f28e1af80062829f721144ff3944977c2
Rob Schluntz <saitou1024@gmail.com>
2015-10-17 14:40:53 -0400

    read random facts and names from file

--- src/main/scala/game/Enemy.scala
+++ src/main/scala/game/Enemy.scala
@@ -4,13 +4,37 @@ package game
 import IDMap._
 import lib.game.GameConfig.{Width}
 
+import java.util.Scanner
+import java.io.File
+
+import lib.util.rand
+
 trait EnemyType extends CharacterType {
 
 }
 
+object Enemy {
+  private lazy val names = read("names.txt")
+  private lazy val facts = read("true-facts.txt")
+
+  def read(filename: String): List[String] = {
+    var lst = List[String]()
+    val sc = new Scanner(new File(filename))
+    while (sc.hasNextLine) {
+      lst = sc.nextLine::lst 
+    }
+    lst
+  }
+
+  def name() = names(rand(names.length-1))
+  def fact() = facts(rand(facts.length-1))  
+}
+
 abstract class Enemy(xc: Float, yc: Float, override val base: EnemyType) extends game.Character(xc, yc, base) {
-  def age: Int
-  def fact: String
+  val age: Int = rand(10, 15)
+
+  val name = Enemy.name
+  val fact = Enemy.fact
 
   def move() = {
     
