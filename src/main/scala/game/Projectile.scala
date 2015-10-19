0f6ee7f5d27c1f112729f756e73e423a7defec3a
Rob Schluntz <saitou1024@gmail.com>
2015-10-18 22:19:55 -0400

    projectiles move and check for intersection

--- src/main/scala/game/Projectile.scala
+++ src/main/scala/game/Projectile.scala
@@ -2,11 +2,28 @@ package com.github.fellowship_of_the_bus
 package eshe
 package game
 
-object Projectile {
+import lib.math.Rect.intersect
 
+object Projectile {
+  val attack = 3
 }
 
 class Projectile(xc: Float, yc: Float, val id: Int, dir: Int) extends GameObject(xc, yc) {
   def move() = x = x + dir
+
+  val attack = Projectile.attack
+
+  override def update(delta: Long, game: Game) = {
+    super.update(delta, game)
+
+    move
+    for (p <- game.players) {
+      if (intersect(this, p)) {
+        hit(p)
+        inactivate
+      }
+    }
+    img.update(delta)
+  }
 }
 
